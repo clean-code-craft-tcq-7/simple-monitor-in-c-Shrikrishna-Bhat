102e15df7facfd2fee850cd7f8f796b8376da69d
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-12-12 11:30:12 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -2,21 +2,21 @@
 #define BATTERY_SOC(soc)                 (((soc) < 20) || ((soc) > 80))
 #define BATTERY_CHARGERATE(chargeRate)   (chargeRate > 0.8)
 
-static int IsTemperatureOK(float temperature)
+int IsTemperatureOK(float temperature)
 {
   if(BATTERY_TEMPERATURE(temperature))
     printf("Temperature out of range!\n");
     return 0;
 }
 
-static int IsSocOK(float soc)
+int IsSocOK(float soc)
 {
   if(BATTERY_SOC(soc))
     printf("State of Charge out of range!\n");
     return 0;
 }
 
-static int IsChargeRate(float chargeRate)
+int IsChargeRate(float chargeRate)
 {
   if(BATTERY_CHARGERATE(chargeRate))
     printf("Charge Rate out of range!\n");
