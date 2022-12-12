e3451a3959f06e58b30ebb0d281f20d8490b758d
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-12-12 15:57:36 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,29 +3,35 @@
 
 int IsTemperatureOK(float temperature)
 {
+  int returnValue = 1;
   if(((temperature) < 0) || ((temperature) > 45))
   {
     printf("Temperature out of range!\n");
-    return 0;
+    returnValue = 0;
   }
+  return returnValue;
 }
 
 int IsSocOK(float soc)
 {
+  int returnValue = 1;
   if(((soc) < 20) || ((soc) > 80))
   {
     printf("State of Charge out of range!\n");
-    return 0;
+    returnValue = 0;
   }
+  return returnValue;
 }
 
 int IsChargeRate(float chargeRate)
 {
+  int returnValue = 1;
   if(chargeRate > 0.8)
   {
     printf("Charge Rate out of range!\n");
-    return 0;
+    returnValue = 0;
   }
+  return returnValue;
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) 
