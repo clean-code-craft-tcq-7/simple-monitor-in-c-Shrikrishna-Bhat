d23e1d1080dc120683f82758e75615e1d4145687
Shrikrishna-Bhat <118532304+Shrikrishna-Bhat@users.noreply.github.com>
2022-12-12 10:24:27 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -5,37 +5,37 @@
 #define BATTERY_SOC(soc)                 (((soc) < 20) || ((soc) > 80))
 #define BATTERY_CHARGERATE(chargeRate)   (chargeRate > 0.8)
 
-static int IsTemperatureOK(float temperature)
+static void IsTemperatureOK(float temperature)
 {
   if(BATTERY_TEMPERATURE(temperature))
   {
     printf("Temperature out of range!\n");
   }
-  return 0;
 }
 
-static int IsSocOK(float soc)
+static void IsSocOK(float soc)
 {
   if(BATTERY_SOC(soc))
   {
     printf("State of Charge out of range!\n");
   }
-  return 0;
 }
 
-static int IsChargeRate(float chargeRate)
+static void IsChargeRate(float chargeRate)
 {
   if(BATTERY_CHARGERATE(chargeRate))
   {
     printf("Charge Rate out of range!\n");
   }
-  return 0;
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   if(IsTemperatureOK(temperature)) {
+    return 0;
   } else if(IsSocOK(soc)) {
+    return 0;
   } else if(IsChargeRate(chargeRate)) {
+    return 0;
   }
   return 1;
 }
